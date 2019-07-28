71d63e9e0ef4658a1079d3fe58c1f17f5a4a2543
anhquoc4062 <anhquoc4062@gmail.com>
2019-07-28 11:42:00 +0700

    add share facebook

--- CoffeeShopProject/wwwroot/user/js/detail_product.js
+++ CoffeeShopProject/wwwroot/user/js/detail_product.js
@@ -1,39 +1,47 @@
 function initFBServer() {
     window.fbAsyncInit = function() {
         FB.init({
-          appId      : '1981611578625971',
-          xfbml      : true,
-          version    : 'v2.3'
+            appId: '1981611578625971',
+            xfbml: true,
+            version: 'v2.4'
         });
-      };
+    };
 
-    (function(d, s, id){
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s);
+        js.id = id;
         js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+    }(document, 'script', 'facebook-jssdk'));
 }
+
 function ShareToFacebook() {
-    $(document).ready(function () {
-        $('#share_facebook').click(function (e) {
-            //e.preventDefault();
-            console.log(GLOBAR_VAR.domain_name);
-             FB.ui({
+    $(document).ready(function() {
+        $('#share_facebook').click(function(e) {
+            e.preventDefault();
+            FB.init({
+                appId: '1981611578625971',
+                xfbml: true,
+                version: 'v2.4'
+            });
+
+            console.log(GLOBAL_VAR.domain_name);
+            FB.ui({
                 method: 'share_open_graph',
                 action_type: 'og.shares',
                 action_properties: JSON.stringify({
-                    object : {
-                       'og:url': GLOBAR_VAR.domain_name + '/SingleProduct/Index/3',
-                       'og:title': 'Test Title',
-                       'og:description': 'Dialogs provide a simple, consistent interface for applications to interface with users.',
-                       'og:image': GLOBAR_VAR.domain_name + '/uploads/product/Americano.png',
-                       'og:og:image:width': '300',
-                       'og:image:height': '300',
+                    object: {
+                        'og:url': 'https://tlecoffeeshop.azurewebsites.net/chi-tiet/coffee/cappuchino-1',
+                        'og:title': 'Test Title',
+                        'og:description': 'Dialogs provide a simple, consistent interface for applications to interface with users.',
+                        'og:image': 'https://tlecoffeeshop.azurewebsites.net/uploads/product/cappuccino_PNG26.png',
+                        'og:image:width': '300',
+                        'og:image:height': '300',
                     }
                 })
-             });
+            });
 
             /*var link = "https://127.0.0.1:5001/SingleProduct/Index/3";
             var desc = "your caption here";
@@ -60,9 +68,9 @@ function ShareToFacebook() {
         });
 
     });
-} 
+}
 
-$(document).ready(function () {
+$(document).ready(function() {
     initFBServer();
 
     ShareToFacebook();
