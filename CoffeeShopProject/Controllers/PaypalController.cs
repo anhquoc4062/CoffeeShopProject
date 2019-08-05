94b71b1a6e3cc1aa14707324696d321a85601050
anhquoc4062 <anhquoc4062@gmail.com>
2019-08-05 19:58:08 +0700

    test .net paypal sdk

---
+++ CoffeeShopProject/Controllers/PaypalController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BraintreeHttp;
+using CoffeeShopProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using PayPal.Core;
+using PayPal.v1.Payments;
+
+namespace CoffeeShopProject.Controllers
+{
+    public class PaypalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<CartItem> Cart
+        {
+            get
+            {
+                var data = SessionHelper.Get<List<CartItem>>(HttpContext.Session, "cart");
+                if (data == null)
+                {
+                    data = new List<CartItem>();
+                }
+
+                return data;
+            }
+        }
+        public async Task<IActionResult> Checkout()
+        {
+            //SandboxEnvironment(clientId, clientSerect)
+            var environment = new SandboxEnvironment("AWIsppPl1e8gcyxUC9laSS0sTWzs_KMntrb6Z2oS3lwjJCUCZpIRLnvm3MQ73pU-Q8mNOT8dreWQ09I4", "EKqG_fKOrmkzGLDAJ3ghvnQ6HtUMWr0HOcWKCx6a168MV4r2poqggYvYYYyZU4VG3si4pRau-Abqdx2W");
+            var client = new PayPalHttpClient(environment);
+
+            //Đọc thông tin đơn hàng từ Session
+            var itemList = new ItemList()
+            {
+                Items = new List<Item>()
+            };
+
+            var tongTien = Cart.Sum(p => p.GiaBan * p.SoLuong);
+            foreach (var item in Cart)
+            {
+                itemList.Items.Add(new Item()
+                {
+                    Name = item.TenThucDon,
+                    Currency = "USD",
+                    Price = item.GiaBan.ToString(),
+                    Quantity = item.SoLuong.ToString(),
+                    Sku = "sku",
+                    Tax = "0"
+                });
+            }
+
+            var payment = new Payment()
+            {
+                Intent = "sale",
+                Transactions = new List<Transaction>()
+                {
+                    new Transaction()
+                    {
+                        Amount = new Amount()
+                        {
+                            Total = tongTien.ToString(),
+                            Currency = "USD",
+                            Details = new AmountDetails
+                            {
+                                Tax = "0",
+                                Shipping = "0",
+                                Subtotal = tongTien.ToString()
+                            }
+                        },
+                        ItemList = itemList,
+                        Description = "Don hang 001",
+                        InvoiceNumber = DateTime.Now.Ticks.ToString()
+                    }
+                },
+                RedirectUrls = new RedirectUrls()
+                {
+                    CancelUrl = DomainName.main_url + "Paypal/Fail",
+                    ReturnUrl = DomainName.main_url + "thanh-toan/AddOrder"
+                },
+                Payer = new Payer()
+                {
+                    PaymentMethod = "paypal"
+                }
+            };
+
+            PaymentCreateRequest request = new PaymentCreateRequest();
+            request.RequestBody(payment);
+
+            try
+            {
+                HttpResponse response = await client.Execute(request);
+                var statusCode = response.StatusCode;
+                Payment result = response.Result<Payment>();
+
+                var links = result.Links.GetEnumerator();
+                string paypalRedirectUrl = null;
+                while (links.MoveNext())
+                {
+                    LinkDescriptionObject lnk = links.Current;
+                    if (lnk.Rel.ToLower().Trim().Equals("approval_url"))
+                    {
+                        //saving the payapalredirect URL to which user will be redirected for payment  
+                        paypalRedirectUrl = lnk.Href;
+                    }
+                }
+
+                return Redirect(paypalRedirectUrl);
+
+            }
+            catch (HttpException httpException)
+            {
+                var statusCode = httpException.StatusCode;
+                var debugId = httpException.Headers.GetValues("PayPal-Debug-Id").FirstOrDefault();
+
+                return RedirectToAction("Fail");
+            }
+
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            //Tạo đơn hàng trong CSDL với trạng thái : Đã thanh toán, phương thức: Paypal
+            return Content("Thanh toán thành công");
+        }
+
+        public IActionResult Fail()
+        {
+            //Tạo đơn hàng trong CSDL với trạng thái : Chưa thanh toán, phương thức: 
+            return Content("Thanh toán thất bại");
+        }
+    }
+}
--- CoffeeShopProject/Helpers/DomainName.cs
+++ CoffeeShopProject/Helpers/DomainName.cs
@@ -1,5 +1,6 @@
 
 public static class DomainName
 {
+    //public static string main_url = "https://localhost:44308/";
     public static string main_url = "https://tlecoffeeshop.azurewebsites.net/";
 }
