317d7836c44d951b7c0db84efcbda53a856513ac
AnhQuoc <anhquoc4062@gmail.com>
2019-08-07 12:05:58 +0700

    Revert "fix order total amount"
    This reverts commit ed187505ddd6474676c307a2fd0c9c8262ef89b3.

--- CoffeeShopProject/wwwroot/user/js/checkout_paypal.js
+++ CoffeeShopProject/wwwroot/user/js/checkout_paypal.js
@@ -1,56 +1,26 @@
 ﻿
-function RenderPaypalButton() {
-    $.ajax({
-        type: "GET",
-        url: '/Cart/LoadCartHidden',
-        dataType: 'json',
-        success: function (data) {
-            var totalAmount = 0;
-            for (i in data) {
-                totalAmount += data[i].soLuong * data[i].giaBan;
-            }
-            paypal.Buttons({
-                createOrder: function (data, actions) {
-                    // Set up the transaction
-                    return actions.order.create({
-                        purchase_units: [{
-                            amount: {
-                                value: '7',
-                                currency_code: 'USD',
-                                breakdown: {
-                                    item_total: {value: '7', currency_code: 'USD'}
-                                }
-                            },
-                            items: [{
-                                name: 'Hafer',
-                                unit_amount: {value: '3', currency_code: 'USD'},
-                                quantity: '1',
-                                sku: 'haf001'
-                            }, {
-                                name: 'Discount',
-                                unit_amount: {value: '4', currency_code: 'USD'},
-                                quantity: '1',
-                                sku: 'dsc002'
-                            }]
-                        }]
-                    });
-                },
-                style: {
-                    color: 'blue',
-                    shape: 'pill',
-                    label: 'pay',
-                    height: 40,
-                    size: 'small'
+
+paypal.Buttons({
+    createOrder: function (data, actions) {
+        // Set up the transaction
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: '1'
                 }
-            }).render('#check-out-paypal');
-        },
-        error: function (error) {
-        }
-    });
-}
+            }]
+        });
+    },
+    style: {
+        color: 'blue',
+        shape: 'pill',
+        label: 'pay',
+        height: 40,
+        size: 'small'
+    }
+}).render('#check-out-paypal');
 
 $(document).ready(function () {
-    RenderPaypalButton();
     $(document).on('click', '#general-checkout-button', function (event) {
         event.preventDefault();
         if ($("#paypal-checkbox").is(":checked")) {
