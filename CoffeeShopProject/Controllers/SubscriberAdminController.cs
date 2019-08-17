325e7bf1560603e77a73b0e62d20fb2722af2ea3
anhquoc4062 <anhquoc4062@gmail.com>
2019-08-17 12:25:31 +0700

    add list subscriber

--- CoffeeShopProject/Controllers/SubscriberAdminController.cs
+++ CoffeeShopProject/Controllers/SubscriberAdminController.cs
@@ -5,13 +5,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using CoffeeShopProject.Common;
 using CoffeeShopProject.Models;
+using MailChimp.Net;
+using MailChimp.Net.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoffeeShopProject.Controllers
 {
-    public class SubscriberAdminController : Controller
+    public class SubscriberAdminController : BaseController
     {
+        
+        private string listId = "f2574fb0f2";
+        private static string apiKey = "b9788ffb309507ffb251f59fc5606d5a-us3";
+        private static readonly MailChimpManager Manager = new MailChimpManager(apiKey);
+
+        public async Task<ActionResult> GetListMember()
+        {
+            var options = new CampaignRequest
+            {
+                ListId = "f2574fb0f2",
+                Status = CampaignStatus.Sent,
+                SortOrder = CampaignSortOrder.DESC,
+                Limit = 10
+            };
+ 
+            try
+            {
+                var model = await Manager.Members.GetAllAsync("f2574fb0f2");
+                return Json(model);
+            }
+            catch (MailChimpException mce)
+            {
+                return Json(mce.Message);
+            }
+            catch(Exception ex)
+            {
+                return Json(ex.Message);
+            }
+        }
         public IActionResult Index()
         {
             return View();
--- CoffeeShopProject/wwwroot/admin/js/subscriber_list.js
+++ CoffeeShopProject/wwwroot/admin/js/subscriber_list.js
@@ -1,21 +1,4 @@
 function initDatatables() {
-    $.ajax({
-        type: "get",
-        url: "https://us3.api.mailchimp.com/3.0/lists/f2574fb0f2/members/",
-        // beforeSend: function(request) {
-        //     request.setRequestHeader("Authorization", "apikey b9788ffb309507ffb251f59fc5606d5a-us3");
-        //     request.setRequestHeader("Access-Control-Allow-Origin", "*");
-        // },
-        headers: {
-             "Authorization": "apikey b9788ffb309507ffb251f59fc5606d5a-us3"
-        },
-        async:true,
-        dataType : 'json',   //you may use jsonp for cross origin request
-        crossDomain:true,
-        success: function (response) {
-            console.log(response);
-        }
-    });
     $('#subscriber_table').DataTable({
         "language": {
             "lengthMenu": "Hiển thị _MENU_ dữ liệu trên trang",
@@ -25,9 +8,70 @@ function initDatatables() {
             "infoFiltered": "(filtered from _MAX_ total records)"
         }
     });
+    var dataRows = [];
+    $.ajax({
+        type: "get",
+        url: "SubscriberAdmin/GetListMember",
+        // beforeSend: function(request) {
+        //     request.setRequestHeader("Authorization", "apikey b9788ffb309507ffb251f59fc5606d5a-us3");
+        //     request.setRequestHeader("Access-Control-Allow-Origin", "*");
+        // },
+        async: true,
+        dataType: 'json', //you may use jsonp for cross origin request
+        success: function(response) {
+            response.forEach(function(member) {
+                var row = [
+                    member.email_address,
+                    member.merge_fields.FNAME + ' ' + member.merge_fields.LNAME,
+                    member.merge_fields.ADDRESS,
+                    member.merge_fields.PHONE,
+                    member.merge_fields.BIRTHDAY,
+                    member.status
+                ];
+                dataRows.push(row);
+            });
+            // $('#subscriber_table').DataTable({
+            //     data: dataRows
+            // });
+            var datatable = $('#subscriber_table').DataTable();
+            datatable.clear();
+            datatable.rows.add(dataRows);
+            datatable.draw();
+        }
+    });
+}
+
+function exportToExcel() {
+    var htmls = "";
+    var uri = 'data:application/vnd.ms-excel;base64,';
+    var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
+    var base64 = function(s) {
+        return window.btoa(unescape(encodeURIComponent(s)))
+    };
+
+    var format = function(s, c) {
+        return s.replace(/{(\w+)}/g, function(m, p) {
+            return c[p];
+        })
+    };
+
+    htmls = $('#subscriber_table')[0].outerHTML;
+    console.log(htmls);
+    var ctx = {
+        worksheet: 'Worksheet',
+        table: htmls
+    };
+
+
+    var link = document.createElement("a");
+    link.download = "export.xls";
+    link.href = uri + base64(format(template, ctx));
+    link.click();
 }
 
 $(document).ready(function() {
     initDatatables();
-
+    $('#export-excel-subscriber').click(function() {
+        exportToExcel();
+    });
 });
